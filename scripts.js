360a8e76472129f1eb6a3972f463fb58277f1d7e
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-23 00:37:58 +0300

    Added functionality: Switching between circles

--- scripts.js
+++ scripts.js
@@ -1,6 +1,28 @@
+// Function to change the image
 function changeImage(imageSrc) {
     document.getElementById('main-img').src = imageSrc;
-    const previews = document.querySelectorAll('.preview');
-    previews.forEach(preview => preview.classList.remove('selected'));
-    event.currentTarget.classList.add('selected');
 }
+
+// Function to change the color of the circle
+function changeColor(selectedColor) {
+    const circles = document.querySelectorAll('.colors-list li');
+
+    circles.forEach(circle => {
+        circle.classList.remove('selected');
+    });
+
+    selectedColor.classList.add('selected');
+
+    const colorClass = selectedColor.classList[1]; 
+    const newImageSrc = colorImages[colorClass];
+
+    changeImage(newImageSrc);
+}
+
+// Event handlers for each circle
+const circles = document.querySelectorAll('.colors-list li');
+circles.forEach(circle => {
+    circle.addEventListener('click', function() {
+        changeColor(this); // Passing the selected circle to the function
+    });
+});
