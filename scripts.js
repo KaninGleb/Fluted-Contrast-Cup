255f7a919d95d49a673307f9298db213f0468f9d
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-24 01:36:57 +0300

    Added functionality: switching the colors of the main mug

--- scripts.js
+++ scripts.js
@@ -13,15 +13,24 @@ function changeColor(selectedColor) {
 
     selectedColor.classList.add('selected');
 
-    const colorClass = selectedColor.classList[1]; 
-    const newImageSrc = colorImages[colorClass];
+    // Изображения для каждого цвета
+    const colorImages = {
+        'blue': './assets/image/main-cup-1/main-cup-1-blue.png',
+        'green': './assets/image/main-cup-1/main-cup-1-green.png',
+        'red': './assets/image/main-cup-1/main-cup-1-red.png',
+        'black': './assets/image/main-cup-1/main-cup-1-black.png',
+        'yellow': './assets/image/main-cup-1/main-cup-1-yellow.png',
+    };
 
-    changeImage(newImageSrc);
+    const colorClass = selectedColor.querySelector('.circle').classList[1]; // Получаем класс цвета
+    const newImageSrc = colorImages[colorClass];
+    if (newImageSrc) {
+        changeImage(newImageSrc); // Меняем изображение
+    }
 }
 
 // Event handlers for each circle
-const circles = document.querySelectorAll('.colors-list li');
-circles.forEach(circle => {
+document.querySelectorAll('.colors-list li').forEach(circle => {
     circle.addEventListener('click', function() {
         changeColor(this); // Passing the selected circle to the function
     });
